9b751900acf10b86e33f91dd42835ea2b5d17b3b
Ryan <coap.ryanmontgomery@gmail.com>
2024-05-14 19:27:37 -0600

    js cont lab done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+// Declare a variable in global scope
+var customerName = 'bob';
+
+// Function to uppercase the global customerName variable
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function to declare a global variable and assign it to 'not bob'
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+// Function to overwrite the bestCustomer variable
+function overwriteBestCustomer() {
+  window.bestCustomer = 'maybe bob';
+}
+
+// Declare a constant in global scope
+const leastFavoriteCustomer = 'someone';
+
+// Function that attempts to change the constant leastFavoriteCustomer
+function changeLeastFavoriteCustomer()
+ {
+   
+    leastFavoriteCustomer = 'new least favorite'; // This will throw an error
+  
+};
+
