6fa2e7468755f1f141fd819c34b8598300c72ac0
Ryan <coap.ryanmontgomery@gmail.com>
2024-08-09 18:12:22 -0600

    l

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 // Declare a variable in global scope
 var customerName = 'bob';
 
-// Function to uppercase the global customerName variable
 function upperCaseCustomerName() {
   customerName = customerName.toUpperCase();
+  return customerName
 }
 
-// Function to declare a global variable and assign it to 'not bob'
 function setBestCustomer() {
-  window.bestCustomer = 'not bob';
+  bestCustomer = 'not bob';
 }
 
-// Function to overwrite the bestCustomer variable
 function overwriteBestCustomer() {
-  window.bestCustomer = 'maybe bob';
+  bestCustomer = 'maybe bob';
 }
 
-// Declare a constant in global scope
-const leastFavoriteCustomer = 'someone';
 
-// Function that attempts to change the constant leastFavoriteCustomer
-function changeLeastFavoriteCustomer()
- {
-   
-    leastFavoriteCustomer = 'new least favorite'; // This will throw an error
-  
-};
 
+const leastFavoriteCustomer = 'not really Bob'
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "definitely Bob";
+  return leastFavoriteCustomer
+}
